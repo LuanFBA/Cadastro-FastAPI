9fb5ae3551c77cad7f2991390f2e42b327a87709
Luan Felipe <luanfba.araujo@hotmail.com>
2022-11-11 12:52:05 -0300

    feat: funcionalidade remover cliente

--- main.py
+++ main.py
@@ -1,70 +1,54 @@
-from uuid import UUID
+from fastapi import Depends, FastAPI, HTTPException, status
+from sqlalchemy.orm import Session
 
-from fastapi import FastAPI, HTTPException
+import crud
+import models
+import schemas
+from database import SessionLocal, engine
 
-from models import Cliente, ClienteUpdate
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-db: list[Cliente] = [
-    Cliente(
-        id=UUID("a353965a-5e92-4b2b-9df8-77725f6c50d3"),
-        nome="Luan",
-        sobrenome="Felipe",
-        cpf="111.222.333-44",
-        telefone="(99)98877-6655",
-        email="teste@teste.com"
-        ),
-    Cliente(
-        id=UUID("2419b208-cb06-49cf-8b89-d452a08fd2d6"),
-        nome="Marcell",
-        sobrenome="Brilhante",
-        cpf="111.222.333-55",
-        telefone="(99)98877-7788",
-        email="teste2@teste2.com"
-        )
-]
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.get("/")
 def root():
     return {"mensagem": "Hello world"}
 
-@app.get("/api/v1/clientes")
-def listar_clientes():
-    return db
+@app.get("/api/v1/clientes", response_model=list[schemas.Cliente])
+def listar_clientes(db: Session=Depends(get_db), skip: int = 0, limit: int = 100):
+    clientes = crud.listar_clientes(db, skip=skip, limit=limit)
+    return clientes
 
-@app.post("/api/v1/clientes")
-def cadastrar_cliente(cliente: Cliente):
-    db.append(cliente)
-    return {"id": cliente.id}
-
-@app.delete("/api/v1/clientes/{cliente_id}")
-def remover_cliente(cliente_id: UUID):
-    for cliente in db:
-        if cliente.id == cliente_id:
-            db.remove(cliente)
-            return "Cliente removido"
+@app.get("/api/v1/clientes/{cliente_id}")
+def obter_cliente(cliente_id: int, db: Session=Depends(get_db)):
+    cliente = crud.obter_cliente(db=db, cliente_id=cliente_id)
+    if cliente is not None:
+        return cliente
     raise HTTPException(
-        status_code=404,
+        status_code=status.HTTP_404_NOT_FOUND,
         detail=f"Cliente com id: {cliente_id} não existe"
     )
 
-@app.put("/api/v1/clientes/{cliente_id}")
-def alterar_cliente(cliente_update: ClienteUpdate, cliente_id: UUID):
-    for cliente in db:
-        if cliente.id == cliente_id:
-            if cliente_update.nome is not None:
-                cliente.nome = cliente_update.nome
-            if cliente_update.sobrenome is not None:
-                cliente.sobrenome = cliente_update.sobrenome
-            if cliente_update.cpf is not None:
-                cliente.cpf = cliente_update.cpf
-            if cliente_update.telefone is not None:
-                cliente.telefone = cliente_update.telefone
-            if cliente_update.email is not None:
-                cliente.email = cliente_update.email
-            return "Cliente alterado"
+@app.post("/api/v1/clientes", response_model=schemas.Cliente, status_code=status.HTTP_201_CREATED)
+def cadastrar_cliente(cliente: schemas.ClienteCreate, db:Session=Depends(get_db)):
+    return crud.cadastrar_cliente(db=db, cliente=cliente)
+
+@app.delete("/api/v1/clientes/{cliente_id}")
+def remover_cliente(cliente_id: int ,db: Session=Depends(get_db)):
+    cliente = crud.obter_cliente(db=db, cliente_id=cliente_id)
+    if cliente is not None:
+        return crud.remover_cliente(db=db, cliente_id=cliente_id)
     raise HTTPException(
-        status_code=404,
+        status_code=status.HTTP_404_NOT_FOUND,
         detail=f"Cliente com id: {cliente_id} não existe"
     )
