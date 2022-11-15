c212ca99125faed5567122e18e594f53ba6f30c9
Luan Felipe <luanfba.araujo@hotmail.com>
2022-11-15 14:21:49 -0300

    build: inclusão de schemas usuario

--- schemas.py
+++ schemas.py
@@ -16,3 +16,28 @@ class Cliente(ClienteBase):
 
     class Config:
         orm_mode = True
+
+class UsuarioBase(BaseModel):
+    nome: str
+    email: str
+    senha: str
+
+class UsuarioCreate(UsuarioBase):
+    pass
+
+class Usuario(UsuarioBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+class UsuarioDB(Usuario):
+    hashed_senha: str
+
+class LoginData(BaseModel):
+    senha: str
+    email: str
+
+class LoginSucesso(BaseModel):
+    usuario: Usuario
+    access_token: str
